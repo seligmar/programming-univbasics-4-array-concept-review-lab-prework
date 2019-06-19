ba82fa437ecc7561add7123f39265e2acfd43fc7
Mary Selig <seligmar@gmail.com>
2019-06-19 13:33:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,6 +2,8 @@ def find_element_index(array, value_to_find)
   # Add your solution here
 end
 
+The first method, find_element_index(array, value_to_find), takes in two parameters, an array of integers and a value to find. This method should return the index of the value that was passed in. If the value is not found, this method should return nil.
+
 def find_max_value(array)
   # Add your solution here
 end
