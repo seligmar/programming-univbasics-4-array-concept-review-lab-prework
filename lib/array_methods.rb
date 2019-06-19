0b68df5b7b105233677ae57f7bd9088a69be5f18
Mary Selig <seligmar@gmail.com>
2019-06-19 13:42:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
   array.each do |value| 
     if value == value_to_find 
-      return array.find_index(value_to_find)
-end
+      array.find_index(value_to_find)
 else nil 
+end
 end 
 end 
 
